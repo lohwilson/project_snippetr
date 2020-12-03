ada256865d2f98a9a95634435339947987d0d27f
Wil Son <lohwilson@hotmail.com>
2020-12-03 10:37:15 +0800

    added auth context for snippet page

--- frontend/src/components/snippets/Snippet.js
+++ frontend/src/components/snippets/Snippet.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import TextareaAutosize from "@material-ui/core/TextareaAutosize";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import { AuthContext } from "./auth/AuthProvider";
 
 export class Snippets extends Component {
   constructor(props) {
@@ -12,9 +13,11 @@ export class Snippets extends Component {
       title: "",
       story: "",
       image: "",
+      user: "",
       editing: false,
     };
   }
+  static contextType = AuthContext;
 
   componentDidMount() {
     console.log(this.props.match.params.id);
@@ -51,16 +54,18 @@ export class Snippets extends Component {
   };
 
   handleUpdate = () => {
-    console.log('updating snippet');
-  }
+    console.log("updating snippet");
+  };
 
   render() {
-    const { title, story, image } = this.state;
+    const { title, story, image, user } = this.state;
+    const { username } = this.context;
     return (
       <div className="container">
         <div>
           <h1>{title}</h1>
           <h3>{story}</h3>
+          <h4>{user}</h4>
           <img src={image} alt="userImage" />
         </div>
 
@@ -69,8 +74,24 @@ export class Snippets extends Component {
             <h1>{title}</h1>
             <h3>{story}</h3>
             <img src={image} alt="userImage" />
-            <Button onClick={this.handleDelete} variant="contained" color="secondary">Delete</Button>
-            <Button onClick={this.handleEdit} variant="contained" color="primary">Edit</Button>
+            {user === username && (
+              <React.Fragment>
+                <Button
+                  onClick={this.handleDelete}
+                  variant="contained"
+                  color="secondary"
+                >
+                  Delete
+                </Button>
+                <Button
+                  onClick={this.handleEdit}
+                  variant="contained"
+                  color="primary"
+                >
+                  Edit
+                </Button>
+              </React.Fragment>
+            )}
           </React.Fragment>
         ) : (
           <React.Fragment>
@@ -115,7 +136,9 @@ export class Snippets extends Component {
                 </Button>
               </div>
             </form>
-            <Button variant="contained" onClick={this.handleEdit}>Cancel</Button>
+            <Button variant="contained" onClick={this.handleEdit}>
+              Cancel
+            </Button>
           </React.Fragment>
         )}
 
@@ -131,4 +154,3 @@ export class Snippets extends Component {
 }
 
 export default Snippets;
-
