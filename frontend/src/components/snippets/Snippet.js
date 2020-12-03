1fb9db4c6588e0577881c7862aefe61e73cb0788
Wil Son <lohwilson@hotmail.com>
2020-12-03 10:30:42 +0800

    css and logic for edit and delete snippet

--- frontend/src/components/snippets/Snippet.js
+++ frontend/src/components/snippets/Snippet.js
@@ -1,16 +1,18 @@
 import axios from "axios";
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import TextareaAutosize from "@material-ui/core/TextareaAutosize";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 export class Snippets extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      snippet: {
-        title: "",
-        story: "",
-        image: "",
-      },
+      title: "",
+      story: "",
+      image: "",
+      editing: false,
     };
   }
 
@@ -24,24 +26,36 @@ export class Snippets extends Component {
       });
     });
   }
-  
+
   handleDelete = async () => {
-    console.log('delete snippet');
+    console.log("delete snippet");
     console.log(this.props);
     const id = this.props.match.params.id;
 
-    await axios.delete("http://localhost:4000/snippetr/"+ id)
-    console.log('deleted', id);
-    this.props.history.push('/dashboard')
-
-  }
+    await axios.delete("http://localhost:4000/snippetr/" + id);
+    console.log("deleted", id);
+    this.props.history.push("/dashboard");
+  };
 
   handleEdit = () => {
-    console.log('edit snippet');
+    console.log("edit snippet");
+    this.setState({ editing: !this.state.editing });
+  };
+
+  handleChange = (event) => {
+    this.setState({ [event.target.id]: event.target.value });
+  };
+
+  handleImageChange = (event) => {
+    this.setState({ image: event.target.files[0] });
+  };
+
+  handleUpdate = () => {
+    console.log('updating snippet');
   }
 
   render() {
-    const { title, story, image } = this.state.snippet;
+    const { title, story, image } = this.state;
     return (
       <div className="container">
         <div>
@@ -49,6 +63,62 @@ export class Snippets extends Component {
           <h3>{story}</h3>
           <img src={image} alt="userImage" />
         </div>
+
+        {!this.state.editing ? (
+          <React.Fragment>
+            <h1>{title}</h1>
+            <h3>{story}</h3>
+            <img src={image} alt="userImage" />
+            <Button onClick={this.handleDelete} variant="contained" color="secondary">Delete</Button>
+            <Button onClick={this.handleEdit} variant="contained" color="primary">Edit</Button>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <form onSubmit={this.handleUpdate}>
+              <div>
+                <TextField
+                  label="title"
+                  type="text"
+                  id="title"
+                  value={title}
+                  onChange={this.handleChange}
+                  autoComplete="off"
+                  className="form-control col-6"
+                />
+                <label htmlFor="story">
+                  Story:
+                  <TextareaAutosize
+                    rowsMin={5}
+                    aria-label="minimum height"
+                    id="story"
+                    value={story}
+                    onChange={this.handleChange}
+                  />
+                </label>
+                <br />
+                <label htmlFor="image">Image: </label>
+                <input
+                  type="file"
+                  id="image"
+                  onChange={this.handleImageChange}
+                />
+              </div>
+              <div>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  endIcon={<Icon>send</Icon>}
+                  style={{ margin: "10px" }}
+                >
+                  Update Snippet
+                </Button>
+              </div>
+            </form>
+            <Button variant="contained" onClick={this.handleEdit}>Cancel</Button>
+          </React.Fragment>
+        )}
+
         <button onClick={this.handleDelete}>Delete</button>
         <button onClick={this.handleEdit}>Edit</button>
 
@@ -61,3 +131,4 @@ export class Snippets extends Component {
 }
 
 export default Snippets;
+
